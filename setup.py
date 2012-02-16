92eb05b6c20ffaca290cb3c471d656f7ec81e2cd
Nishad Musthafa <nishadmusthafa@gmail.com>
2012-02-16 18:50:21 +0530

    Changed the reference to the README

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     url='https://github.com/plivo/django-admin-multi-flexselect',
     license='Public Domain',
     description='Dynamic Multiple select fields for the Django Admin that just works combined with chosen.js.',
-    long_description=open('README.markdown').read(),
+    long_description=open('README').read(),
     package_data={'flexselect_multiple': ['static/flexselect_multiple/js/flexselect_multiple.js']},
 )
